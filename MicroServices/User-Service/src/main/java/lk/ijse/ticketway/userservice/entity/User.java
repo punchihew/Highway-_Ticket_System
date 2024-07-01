0aed6f6ba3fc22c98d8b0459d2ff787f567f7464
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-01 19:31:05 +0530

    updeta

--- MicroServices/User-Service/src/main/java/lk/ijse/ticketway/userservice/entity/User.java
+++ MicroServices/User-Service/src/main/java/lk/ijse/ticketway/userservice/entity/User.java
@@ -1,9 +1,32 @@
 package lk.ijse.ticketway.userservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/1/2024
  */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String contact;
+    private String address;
+    private String nic;
 }
