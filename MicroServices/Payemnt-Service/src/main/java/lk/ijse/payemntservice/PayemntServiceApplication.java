28ab089e87fd5eda17764e7c2d0b086b43a73f6e
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-01 20:05:16 +0530

    updeta

---
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/PayemntServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.payemntservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayemntServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayemntServiceApplication.class, args);
+    }
+
+}
