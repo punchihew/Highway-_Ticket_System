c177bf40c468b02524162a07710c237442e1da93
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-01 19:24:20 +0530

    updeta

---
+++ MicroServices/User-Service/src/main/java/lk/ijse/ticketway/userservice/config/WebAppConfig.java
@@ -0,0 +1,49 @@
+package lk.ijse.ticketway.userservice.config;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 7/1/2024
+ */
+@RestController
+@RequestMapping("/actions")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping("/save")
+    public ResponseDTO add(@RequestBody TicketDTO ticketDTO) {
+        try {
+            return ticketService.save(ticketDTO);
+        } catch (Exception e) {
+            return new ResponseDTO(e.getMessage(), 500);
+
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseDTO update(@RequestBody TicketDTO ticketDTO) {
+        try {
+            return ticketService.update(ticketDTO);
+        } catch (Exception e) {
+            return new ResponseDTO(e.getMessage(), 500);
+        }
+    }
+
+    @GetMapping
+    public ResponseDTO get() {
+        try {
+            return ticketService.getAll();
+        } catch (Exception e) {
+            return new ResponseDTO(e.getMessage(), 500);
+        }
+    }
+
+}
