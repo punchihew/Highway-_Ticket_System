3e9405887d817de8c5c9a653df8a67b96c73716d
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-01 20:07:57 +0530

    updeta

--- MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/controller/PaymentController.java
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/controller/PaymentController.java
@@ -1,9 +1,15 @@
 package lk.ijse.payemntservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/1/2024
  */
+
+@RestController
+@RequestMapping("/actions")
 public class PaymentController {
 }
