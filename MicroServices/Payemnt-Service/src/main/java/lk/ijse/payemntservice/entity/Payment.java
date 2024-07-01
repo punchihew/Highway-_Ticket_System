bc05ec0f0ced4289f164d346887c1336b5ff67eb
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-01 20:08:47 +0530

    updeta

--- MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/entity/Payment.java
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/entity/Payment.java
@@ -1,9 +1,21 @@
 package lk.ijse.payemntservice.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/1/2024
  */
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
 }
