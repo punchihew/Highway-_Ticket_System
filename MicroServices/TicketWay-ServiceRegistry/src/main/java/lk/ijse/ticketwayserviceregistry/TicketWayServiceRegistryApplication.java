1a3bc87b28d34118ad344de8ba9522d69d0ce711
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-01 19:46:40 +0530

    updeta

--- MicroServices/TicketWay-ServiceRegistry/src/main/java/lk/ijse/ticketwayserviceregistry/TicketWayServiceRegistryApplication.java
+++ MicroServices/TicketWay-ServiceRegistry/src/main/java/lk/ijse/ticketwayserviceregistry/TicketWayServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.ticketwayserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TicketWayServiceRegistryApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class TicketWayServiceRegistryApplication {
     }
 
 }
+
