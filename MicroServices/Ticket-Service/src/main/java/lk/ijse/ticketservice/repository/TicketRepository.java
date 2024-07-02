7dbef3941b31d02f5e0560b9853aed3360543a65
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:47:12 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/repository/TicketRepository.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/repository/TicketRepository.java
@@ -1,5 +1,6 @@
 package lk.ijse.ticketservice.repository;
 
+import lk.ijse.ticketservice.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
  * @author: Navishka
  * @date: 7/2/2024
  */
-
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
 }
 
