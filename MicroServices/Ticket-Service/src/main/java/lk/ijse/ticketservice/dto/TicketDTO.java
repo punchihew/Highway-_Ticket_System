290ba6c6cb4e392bc9c08007ced748207221d885
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:45:55 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/dto/TicketDTO.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/dto/TicketDTO.java
@@ -1,9 +1,23 @@
 package lk.ijse.ticketservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/2/2024
  */
+
 public class TicketDTO {
+    private Long id;
+    private String ticketId;
+    private String ticketType;
+    private String ticketPrice;
+    private String ticketQty;
+    private String ticketDate;
+    private String ticketTime;
+    private String ticketStatus;
+    private String ticketLocation;
 }
