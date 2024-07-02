a12684c2b4e4ac466e0773090f6e027233cc8505
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:48:12 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/util/ResponseDTO.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/util/ResponseDTO.java
@@ -1,9 +1,21 @@
 package lk.ijse.ticketservice.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/2/2024
  */
+
 public class ResponseDTO {
+    private int status;
+    private String message;
+    private HashMap<String, Object> data = new HashMap<>();
+
+
 }
