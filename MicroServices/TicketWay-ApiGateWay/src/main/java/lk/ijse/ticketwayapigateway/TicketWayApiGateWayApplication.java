064225ea7f23aea7a533118d618783204f53d902
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:56:29 +0530

    updeta

---
+++ MicroServices/TicketWay-ApiGateWay/src/main/java/lk/ijse/ticketwayapigateway/TicketWayApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticketwayapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketWayApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketWayApiGateWayApplication.class, args);
+    }
+
+}
---
+++ MicroServices/TicketWay-ApiGateWay/src/test/java/lk/ijse/ticketwayapigateway/TicketWayApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticketwayapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketWayApiGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
