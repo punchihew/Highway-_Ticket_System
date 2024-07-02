2e4ac8c5cf184625030517c972fb4bf59580d012
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:57:33 +0530

    updeta

--- MicroServices/TicketWay-ApiGateWay/src/main/java/lk/ijse/ticketwayapigateway/TicketWayApiGateWayApplication.java
+++ MicroServices/TicketWay-ApiGateWay/src/main/java/lk/ijse/ticketwayapigateway/TicketWayApiGateWayApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.ticketwayapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TicketWayApiGateWayApplication {
 
     public static void main(String[] args) {
