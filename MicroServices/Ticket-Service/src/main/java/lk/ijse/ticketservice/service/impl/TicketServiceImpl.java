e3334482adc4e6bfa28c9bfff998a25f919a863f
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:50:51 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/service/impl/TicketServiceImpl.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/service/impl/TicketServiceImpl.java
@@ -1,9 +1,26 @@
 package lk.ijse.ticketservice.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/2/2024
  */
-public class TicketServiceImpl {
+@Service
+public class TicketServiceImpl implements TicketService {
+
+    private final TicketRepository ticketRepository;
+    private final ModelMapper mapper;
+
+
+
+
+
+
+
 }
