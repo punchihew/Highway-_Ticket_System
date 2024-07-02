a2e3756c6ca4a46036500394dd9b0a2fde83beae
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:44:42 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/config/WebAppConfig.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/config/WebAppConfig.java
@@ -1,9 +1,19 @@
 package lk.ijse.ticketservice.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/2/2024
  */
+
 public class WebAppConfig {
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 }
