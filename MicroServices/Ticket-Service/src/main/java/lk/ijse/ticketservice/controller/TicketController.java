7e29f1957aa68e13c80cd94e33dc67de2fa8af06
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:52:37 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/controller/TicketController.java
@@ -1,9 +1,21 @@
 package lk.ijse.ticketservice.controller;
 
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/2/2024
  */
+
 public class TicketController {
+
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+
+
 }
