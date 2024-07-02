058e29ec0bb2e10b5b1ca01b4965cef0e4429fc5
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-02 11:46:29 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/entity/Ticket.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/entity/Ticket.java
@@ -1,9 +1,30 @@
 package lk.ijse.ticketservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/2/2024
  */
+
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ticketId;
+    private String ticketType;
+    private String ticketPrice;
+    private String ticketQty;
+    private String ticketDate;
+    private String ticketTime;
+    private String ticketStatus;
+    private String ticketLocation;
 }
