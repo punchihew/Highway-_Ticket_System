e2062425f9cf689cea734e19e7e2c7629cde4047
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:46:47 +0530

    updeta

--- MicroServices/TicketWay-ConfigServer/src/main/java/lk/ijse/ticketway/configserver/entity/Payment.java
+++ MicroServices/TicketWay-ConfigServer/src/main/java/lk/ijse/ticketway/configserver/entity/Payment.java
@@ -1,9 +1,42 @@
 package lk.ijse.ticketway.configserver.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paymentId;
+    private String paymentType;
+    private Double paymentAmount;
+    private String paymentTime;
+    private String paymentStatus;
+    private String paymentLocation;
+    @CreationTimestamp
+    private Timestamp date;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ticket_id", referencedColumnName = "id")
+    private Ticket ticket;
 }
+
