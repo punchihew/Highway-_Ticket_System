5cf77eea3153683a138de41f8f6fe48530071f3a
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 21:32:06 +0530

    updeta

--- MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/service/PaymentService.java
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/service/PaymentService.java
@@ -2,14 +2,66 @@ package lk.ijse.payemntservice.service;
 
 import lk.ijse.payemntservice.dto.PaymentDTO;
 import lk.ijse.payemntservice.util.ResponseDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
-public interface PaymentService {
-    ResponseDTO add(PaymentDTO paymentDTO);
+@Service
+public class PaymentServiceImpl implements PaymentService {
 
-    ResponseDTO update(PaymentDTO paymentDTO);
+    private final PaymentRepository paymentRepository;
+    private final ModelMapper modelMapper;
+    private final RestTemplate restTemplate;
+    private final Logger logger = Logger.getLogger(PaymentServiceImpl.class.getName());
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository, ModelMapper modelMapper, RestTemplate restTemplate) {
+        this.paymentRepository = paymentRepository;
+        this.modelMapper = modelMapper;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public ResponseDTO add(PaymentDTO paymentDTO) {
+        Payment map = modelMapper.map(paymentDTO, Payment.class);
+        ResponseDTO forObject = restTemplate.getForObject("http://ticketway-ticket-service:8080/api/v1/ticket/actions/get-ticket/" + paymentDTO.getTicketId(), ResponseDTO.class);
+        assert forObject != null;
+        forObject.getData().forEach((k, v) -> {
+            if (k.equals("ticket")) {
+                map.setTicket(modelMapper.map(v, Ticket.class));
+            }
+        });
+        Payment save = paymentRepository.save(map);
+        if (save != null) {
+            logger.info("Payment Saved Successfully");
+        } else {
+            logger.severe("Error in Saving Payment");
+        }
+        return save != null ? new ResponseDTO("Success", 200) : new ResponseDTO("Error", 500);
+    }
+
+    @Override
+    public ResponseDTO update(PaymentDTO paymentDTO) {
+        Payment map = modelMapper.map(paymentDTO, Payment.class);
+        ResponseDTO forObject = restTemplate.getForObject("http://ticketway-ticket-service:8080/api/v1/ticket/actions/get-ticket/" + paymentDTO.getTicketId(), ResponseDTO.class);
+        assert forObject != null;
+        forObject.getData().forEach((k, v) -> {
+            if (k.equals("ticket")) {
+                map.setTicket(modelMapper.map(v, Ticket.class));
+            }
+        });
+        Payment save = paymentRepository.save(map);
+        if (save != null) {
+            logger.info("Payment Updated Successfully");
+        } else {
+            logger.severe("Error in Updating Payment");
+        }
+        return save != null ? new ResponseDTO("Success", 200) : new ResponseDTO("Error", 500);
+    }
 }
