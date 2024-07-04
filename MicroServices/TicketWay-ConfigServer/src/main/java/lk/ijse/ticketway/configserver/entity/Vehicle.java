3b8ddcad75edf36307de4659a4d98ff0137daf6d
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:47:57 +0530

    updeta

--- MicroServices/TicketWay-ConfigServer/src/main/java/lk/ijse/ticketway/configserver/entity/Vehicle.java
+++ MicroServices/TicketWay-ConfigServer/src/main/java/lk/ijse/ticketway/configserver/entity/Vehicle.java
@@ -1,9 +1,47 @@
 package lk.ijse.ticketway.configserver.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String vehicleNumber;
+    private String vehicleType;
+    private String vehicleBrand;
+    private String vehicleModel;
+    private String vehicleYear;
+    private String vehicleColor;
+    private String vehicleFuelType;
+    private String vehicleTransmissionType;
+    private String vehicleEngineCapacity;
+    private String vehicleMileage;
+    private String vehicleStatus;
+    private String vehicleLocation;
+    @CreationTimestamp
+    private Timestamp vehicleTime;
+
+    @OneToMany(mappedBy = "vehicle")
+    private List<Ticket> tickets;
 }
