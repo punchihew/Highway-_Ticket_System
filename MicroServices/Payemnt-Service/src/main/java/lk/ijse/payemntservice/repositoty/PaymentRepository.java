b416568b6ffa80e5beebb63d0eafdd1a0b0e5a13
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 21:29:55 +0530

    updeta

--- MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/repositoty/PaymentRepository.java
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/repositoty/PaymentRepository.java
@@ -1,5 +1,6 @@
 package lk.ijse.payemntservice.repositoty;
 
+import lk.ijse.payemntservice.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
