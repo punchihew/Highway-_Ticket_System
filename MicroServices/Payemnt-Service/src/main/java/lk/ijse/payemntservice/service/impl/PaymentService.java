a7296d3c746b601cd7d3a5b160c61b27085ea313
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 21:33:43 +0530

    updeta

--- MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/service/impl/PaymentService.java
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/service/impl/PaymentService.java
@@ -1,9 +1,15 @@
 package lk.ijse.payemntservice.service.impl;
 
+import lk.ijse.payemntservice.dto.PaymentDTO;
+import lk.ijse.payemntservice.util.ResponseDTO;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
 public interface PaymentService {
+    ResponseDTO add(PaymentDTO paymentDTO);
+
+    ResponseDTO update(PaymentDTO paymentDTO);
 }
