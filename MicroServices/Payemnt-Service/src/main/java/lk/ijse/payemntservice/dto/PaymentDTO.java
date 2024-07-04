540e757c14095e3b8d217a43fcc96b452c3bf15c
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 21:25:15 +0530

    updeta

--- MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/dto/PaymentDTO.java
+++ MicroServices/Payemnt-Service/src/main/java/lk/ijse/payemntservice/dto/PaymentDTO.java
@@ -1,9 +1,24 @@
 package lk.ijse.payemntservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDTO {
+    private Long id;
+    private String paymentId;
+    private String paymentType;
+    private Double paymentAmount;
+    private String paymentTime;
+    private String paymentStatus;
+    private String paymentLocation;
+    private Long ticketId;
 }
