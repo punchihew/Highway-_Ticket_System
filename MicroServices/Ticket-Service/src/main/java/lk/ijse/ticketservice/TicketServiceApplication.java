1eb1242d3ffcaef658e5c6ddd14789e8acb80039
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 21:22:15 +0530

    updeta

--- MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/TicketServiceApplication.java
+++ MicroServices/Ticket-Service/src/main/java/lk/ijse/ticketservice/TicketServiceApplication.java
@@ -2,6 +2,9 @@ package lk.ijse.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TicketServiceApplication {
@@ -10,4 +13,9 @@ public class TicketServiceApplication {
         SpringApplication.run(TicketServiceApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
